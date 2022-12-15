9ee83ddc1f2a526b8d339df7bc1b6a5791b65851
Siarhei Saroka <siarhei_saroka@epam.com>
2022-12-15 23:24:29 +0300

    main part done

--- array.sh
+++ array.sh
@@ -1,3 +1,28 @@
 #!/bin/bash
 
-# Place your code here
+arr=({a..z})
+#echo "${arr[0]}"
+
+for d in folder_{a..z}; do 
+    if [[ -d "$d" ]]; then
+        echo "$d exists on your filesystem. Removing $d"
+        rmdir "./$d"
+    fi 
+done
+arg0=$1
+arg1=${arg0:=0}
+echo "arg1:"$arg1
+
+for((ind=0;ind<arg1;ind++)); do
+    mkdir "./folder_${arr[ind]}"
+done
+
+
+
+
+    
+
+
+
+    
+
