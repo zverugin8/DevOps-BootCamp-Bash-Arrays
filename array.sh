cad0af8c3c7e668e62d93ba8c2cbfb2f6eb25a2e
Sergey Soroka <zverugin8@gmail.com>
2022-12-16 10:04:38 +0300

    output text message added

--- array.sh
+++ array.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 arr=({a..z})
-#echo "${arr[0]}"
 
 for d in folder_{a..z}; do 
     if [[ -d "$d" ]]; then
@@ -11,11 +10,17 @@ for d in folder_{a..z}; do
 done
 arg0=$1
 arg1=${arg0:=0}
-echo "arg1:"$arg1
+res_str=""
+if [[ $arg0 -eq 1 ]]; then many=""
+else many="s"; fi
+
 
 for((ind=0;ind<arg1;ind++)); do
     mkdir "./folder_${arr[ind]}"
+    if (( ind<arg1-1 )); then res_str+="folder_${arr[ind]}, "
+    else res_str+="folder_${arr[ind]}"; fi
 done
+echo "$arg0 folder$many created: $res_str"
 
 
 
